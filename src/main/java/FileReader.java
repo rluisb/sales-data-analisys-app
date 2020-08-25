343366813baf5b178108b95c6e3f7c1a1069a73d
Ricardo Conceição <ricardo220893@gmail.com>
2020-08-25 15:29:34 -0300

    Implementing the continuous file reading

--- src/main/java/FileReader.java
+++ src/main/java/FileReader.java
@@ -1,11 +1,8 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.List;
 
 public class FileReader {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         String homePath = System.getProperty("user.home");
         String dataPath = "data";
         String inputDir = "in";
@@ -13,22 +10,59 @@ public class FileReader {
 
         String processingStatus = "-processing";
 
-        Path path = Paths.get(homePath, dataPath, inputDir, "test2.txt");
+        Path path = Paths.get(homePath, dataPath, inputDir);
 
-        List<String> fileContent = Files.readAllLines(path);
+        if (!Files.exists(path)) {
+            throw new Exception("Directory doesn't exists");
+        }
 
-        String processingFileName = path.getFileName().toString().concat(processingStatus);
+//        List<String> fileContent = Files.readAllLines(path);
+//
+//        String processingFileName = path.getFileName().toString().concat(processingStatus);
+//
+//        Files.move(path, path.resolveSibling(processingFileName));
+//
+//        Path processingFilePath = Paths.get(homePath, dataPath, inputDir, processingFileName);
+//
+//        fileContent.stream()
+//                .forEach(System.out::println);
+//
+//        Path processedFilePath = Paths.get(homePath, dataPath, outputDir, path.getFileName().toString());
+//
+//        Files.move(processingFilePath, processedFilePath);
 
-        Files.move(path, path.resolveSibling(processingFileName));
+        WatchService watchService
+                = FileSystems.getDefault().newWatchService();
 
-        Path processingFilePath = Paths.get(homePath, dataPath, inputDir, processingFileName);
+        path.register(
+                watchService,
+                StandardWatchEventKinds.ENTRY_CREATE);
 
-        fileContent.stream()
-                .forEach(System.out::println);
+        WatchKey key;
+        while ((key = watchService.take()) != null) {
+            for (WatchEvent<?> event : key.pollEvents()) {
+                String fileName = event.context().toString();
 
-        Path processedFilePath = Paths.get(homePath, dataPath, outputDir, path.getFileName().toString());
+                if (!fileName.contains(processingStatus)) {
+                    Path pathToFile = Paths.get(path.toString(), fileName);
+                    List<String> fileContent = Files.readAllLines(pathToFile);
 
-        Files.move(processingFilePath, processedFilePath);
+                    String processingFileName = fileName.concat(processingStatus);
+
+                    Files.move(pathToFile, pathToFile.resolveSibling(processingFileName));
+
+                    Path processingFilePath = Paths.get(homePath, dataPath, inputDir, processingFileName);
+
+                    fileContent.stream()
+                            .forEach(System.out::println);
+
+                    Path processedFilePath = Paths.get(homePath, dataPath, outputDir, fileName);
+
+                    Files.move(processingFilePath, processedFilePath);
+                }
+            }
+            key.reset();
+        }
     }
 }
 
